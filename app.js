04724036e494a1350a306018dfb6a3af3d625d1c
Rossokha Illia <39594502+in7rique@users.noreply.github.com>
2023-06-28 20:49:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoit...');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
